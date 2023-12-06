95070b1c19606dd178b0512916069ccd6ea56760
Ihor Leonov <ihorleonow@gmail.com>
2023-12-06 14:18:40 +0200

    ADD | new API config

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -13,6 +13,7 @@ export const Header: FC = () => {
   useEffect(() => {
     dispatch(getUAHtoUSD());
     dispatch(getUAHtoEUR());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
