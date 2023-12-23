5f5e9a968e9e185232698e288f19503e578e7f83
Ihor Leonov <ihorleonow@gmail.com>
2023-12-23 21:23:13 +0200

    Update Htag.props.ts

--- src/components/Htag/Htag.props.ts
+++ src/components/Htag/Htag.props.ts
@@ -1,7 +1,7 @@
 import { DetailedHTMLProps, HtmlHTMLAttributes } from "react";
 
-export interface TitleProps
-  extends DetailedHTMLProps<HtmlHTMLAttributes<HTMLTitleElement>, HTMLTitleElement> {
+export interface HtagProps
+  extends DetailedHTMLProps<HtmlHTMLAttributes<HTMLHeadingElement>, HTMLHeadingElement> {
   tag: "h1" | "h2" | "h3";
   children: React.ReactNode;
 }
